5ef91a0a322696a90d24a86e464f7636c8b13e5f
Luca Fabbri <luca@keul.it>
2013-07-04 10:48:25 +0200

    flickering prevention on page load

--- collective/azindexpage/browser/page.js
+++ collective/azindexpage/browser/page.js
@@ -1,4 +1,6 @@
+;$('head').append('<style type="text/css">.azpanes {display:none;}</style>');
 $(function() {
     $("#azindexpage ul.aztabs").tabs("div.azpanes > div");
+    $("#azindexpage .azpanes:hidden").show();
     $("#azindexpage div.noword a.letter-link").click(function(event) {event.preventDefault();});
 });
