662942136f2fd3e78b381444dda8e101ddcf72ce
Luca Fabbri <luca@keul.it>
2013-07-31 11:17:23 +0200

    bug fixed: sometimes reindex was performed before index creation

--- collective/azindexpage/setuphandlers.py
+++ collective/azindexpage/setuphandlers.py
@@ -1,10 +1,13 @@
-
+import logging
 
 def setupVarious(context):
     marker = 'collective.azindexpage.marker.txt'
     if context.readDataFile(marker) is None:
         # Not your add-on
         return
+    logger = logging.getLogger('collective.azindexpage')
     portal = context.getSite()
     catalog = portal.portal_catalog
+    logger.info('Reindexin AXIndex; this can take a while')
     catalog.reindexIndex('AZIndex', portal.REQUEST)
+    logger.info('Done')
